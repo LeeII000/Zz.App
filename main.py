2d231241783effd09eb18d28c0d7efba9f02d8e9
mingyukim7 <117696118+mingyukim7@users.noreply.github.com>
2022-12-12 18:47:58 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.video import Video
+from kivy.core.audio import SoundLoader
 from kivy.properties import StringProperty, BooleanProperty
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.properties import Clock
@@ -59,6 +61,7 @@ class SCManager(ScreenManager):
     pass
             
 class MainApp(App):
+
     pass
 
 MainApp().run()
