b2dd8a825f6be63f3f266044694636beed837c55
5undura <117696168+5undura@users.noreply.github.com>
2022-12-15 09:48:17 +0900

    Add files via upload
    songssss

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.video import Video
-from kivy.core.audio import SoundLoader
+from kivy.core.audio import SoundLoader, Sound
 from kivy.properties import StringProperty, BooleanProperty
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.properties import Clock
@@ -13,13 +13,46 @@ from kivy.graphics import Rectangle
 import math
 
 class FirstScreen(Screen):
-    pass
+    def play_music(self):
+        music = SoundLoader.load('songs/an_ending.mp3')
+
+        if music:
+            music.loop = True
+            music.play()  
 
 class EndingScreen(Screen):
     pass
 
 class OptionScreen(Screen):
-    pass
+    def play1(self):
+        music = SoundLoader.load('songs/an_ending.mp3')
+
+        if self.ids.music1.state == 'down':
+            self.ids.music1.text = "1"
+            music.play()
+        else:
+            music.unload()
+
+    def play2(self):
+        music = SoundLoader.load('songs/rain.mp3')
+
+        if music:
+            music.loop = True
+            music.play()
+    
+    def play3(self):
+        music = SoundLoader.load('songs/3.mp3')
+
+        if music:
+            music.loop = True
+            music.play()
+
+    def play4(self):
+        music = SoundLoader.load('songs/fire.mp3')
+
+        if music:
+            music.loop = True
+            music.play()
 
 class OptionWidget(BoxLayout):
     pass
@@ -28,11 +61,12 @@ class Ingame(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)  
 
-    def play_music(self):
+    def stop_music(self):
         music = SoundLoader.load('songs/an_ending.mp3')
 
-        if music:
-            music.play()         
+        music.play()
+        music.stop()
+        music.unload()
 
 class Sheep(Widget):
 
@@ -62,11 +96,23 @@ class Sheep(Widget):
             y = 50
             self.sheep.pos = (x,y)
 
+class Music(Sound):
+    def music1(self):
+        music1 = SoundLoader.load('songs/an_ending.mp3')
+
+    def music2(self):
+        music2 = SoundLoader.load('songs/rain.mp3')
+
+    def music3(self):
+        music3 = SoundLoader.load('songs/3.mp3')
+
+    def music4(self):
+        music4 = SoundLoader.load('songs/fire.mp3')
+
 class SCManager(ScreenManager):
     pass
             
 class MainApp(App):
-
     pass
 
 MainApp().run()
